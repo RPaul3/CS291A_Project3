0e34f6b22a7c47fc2c1941173025b6fe74f3b1a4
RPaul3 <paulren0908@gmail.com>
2019-10-29 22:59:41 -0700

    App.js mod

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.eventSource = '';
     this.state = {
       showPopup: true,
       history: [],
       users:[]
     };
-    //this.startEventSource = this.startEventSource.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
 
 
@@ -28,69 +26,12 @@ sendMessage = (message) => {
 }
 
 
-/*
-startEventSource(token) {
-  //const msg = this.context
-  //console.log(msg)
-  console.log("eventSource started")
-  this.eventSource = new EventSource(`http://localhost:4567/stream/${token}`);
-  
-  
-   this.eventSource.addEventListener("Disconnect", e =>
-    this.setState({history: [...this.state.history, JSON.parse(e.data).created + " " + "Disconnect"]})
-    );
-
-    this.eventSource.addEventListener("Message", e =>
-      this.setState({history: [...this.state.history, JSON.parse(e.data).created + " " + JSON.parse(e.data).user + " " + JSON.parse(e.data).message + "Message"]})
-    );
-
-    this.eventSource.addEventListener("Join", e =>{
-      var msg = JSON.parse(e.data).created + " " + JSON.parse(e.data).user + " " + "JOIN";
-      this.setState({history: [...this.state.history,msg]})
-    }
-    );
-
-    this.eventSource.addEventListener("Part", e =>
-      this.setState({history: [...this.state.history, JSON.parse(e.data).created + " " + JSON.parse(e.data).user + " " + "Part"]})
-    );
-
-    this.eventSource.addEventListener("ServerStatus", e =>
-      this.setState({history: [...this.state.history, JSON.parse(e.data).created + " " + JSON.parse(e.data).status + " " + "ServerStatus"]})
-    );
-
-    this.eventSource.addEventListener("Users", e =>
-      this.setState({history: [...this.state.history, JSON.parse(e.data).created + " " + JSON.parse(e.data).users + " " + "Users"]})
-    );
-}*/
-
-
-
-handleSubmit(username, password){
-
-
+handleSubmit(){
     this.setState({  
        showPopup: !this.state.showPopup  
     }); 
-    /*
-    var xhr = new XMLHttpRequest();
-    var FD  = new FormData();
-    FD.append('username', username);
-    FD.append('password', password);
-
-    
-    xhr.open("POST", 'http://localhost:4567/login', true);
-    xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 201) {
-       var jsonResponse = JSON.parse(xhr.responseText);
-       this.startEventSource(jsonResponse["token"])
-    }
-  };
-    xhr.send(FD);*/
   }
 
-
-
- 
 
     render() {
     return (
